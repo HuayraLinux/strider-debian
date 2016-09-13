924c3d381931c3a7eff36e2bfc429262b072c926
Ignacio Losiggio <iglosiggio@gmail.com>
2016-09-13 12:29:50 -0300

    script de reporte

--- worker.js
+++ worker.js
@@ -11,20 +11,19 @@ module.exports = {
       prepare: prepareCmd(config, job),
       test: testCmd(config, job),
       deploy: shellCommand(config.deploy, config.shell, job),
-      cleanup: shellCommand(config.cleanup, config.shell, job)
+      cleanup: cleanupCmd(config, job)
     });
   }
 };
 
 
-function prepareCmd(config, job){
+function prepareCmd(config, job) {
   var cmd = (config.useuscan ? 'uscan --force-download && ':'');
 
   // by default, 64b is the preferred arch
-  if( config.arch == 'all' ){
+  if(config.arch === 'all') {
     cmd += 'pdebuild -- --basetgz /var/lib/strider/chroot/' + dconf.chroots.amd64;
-  }
-  else if( config.arch == 'any' ){
+  } else if(config.arch === 'any') {
     cmd += 'pdebuild -- --basetgz /var/lib/strider/chroot/' + dconf.chroots.amd64;
     cmd += ' && pdebuild -- --basetgz -b /var/lib/strider/chroot/' + dconf.chroots.i386;
   }
@@ -32,14 +31,20 @@ function prepareCmd(config, job){
   return shellCommand(cmd, config.shell, job);
 }
 
-function testCmd(config, job){
-  if (config.usepiuparts ){
+function testCmd(config, job) {
+  if(config.usepiuparts) {
     var cmd = '';
     var cmd_get_pkg_filename = '$('+dconf.chroots.get_pkg_filename+' `pwd`)';
     cmd += 'sudo piuparts -d stable /var/lib/strider/chroot/result/'+cmd_get_pkg_filename;
 
     return shellCommand(cmd, config.shell, job);
   }
+}
+
+function cleanupCmd(config, job) {
+  var cmd = '';
+  cmd += __dirname + 'script/report';
+  return shellCommand(cmd, config.shell, job);
 }
 
 function shellCommand(command, shell, job) {
