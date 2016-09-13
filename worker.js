e8dfd4c30fa370ce14677248a045e8ece6ad1c5d
Ignacio Losiggio <iglosiggio@gmail.com>
2016-09-13 14:43:47 -0300

    Sobraba el export también

--- worker.js
+++ worker.js
@@ -42,7 +42,7 @@ function testCmd(config, job) {
 }
 
 function cleanupCmd(config, job) {
-  var cmd = 'export PROJECT_NAME=' + job.project.display_name
+  var cmd = 'PROJECT_NAME=' + job.project.display_name
                 + ' PROJECT_URL=' + job.project.display_url
                 + ' CHANGES_FILE=$(' + __dirname + '/scripts/get-package-filename `pwd`)'
                 + ' ' + __dirname + '/scripts/report\n';
