c24293f6a5f1a7500fbcd281df3adb362003c3aa
Ignacio Losiggio <iglosiggio@gmail.com>
2016-09-13 14:41:13 -0300

    Typo: sobraba un ;

--- worker.js
+++ worker.js
@@ -44,7 +44,7 @@ function testCmd(config, job) {
 function cleanupCmd(config, job) {
   var cmd = 'export PROJECT_NAME=' + job.project.display_name
                 + ' PROJECT_URL=' + job.project.display_url
-                + ' CHANGES_FILE=$(' + __dirname + '/scripts/get-package-filename `pwd`)';
+                + ' CHANGES_FILE=$(' + __dirname + '/scripts/get-package-filename `pwd`)'
                 + ' ' + __dirname + '/scripts/report\n';
   return shellCommand(cmd, config.shell, job);
 }
